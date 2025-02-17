42bb64bb34b19cf226198d40552daad85c962f74
s21sd <srivastavasunny359@gmail.com>
2025-02-17 12:06:07 +0530

    First Commit

--- quizapp/src/main/java/com/quizyfy/quizapp/Controllers/QuizController.java
+++ quizapp/src/main/java/com/quizyfy/quizapp/Controllers/QuizController.java
@@ -3,12 +3,18 @@ package com.quizyfy.quizapp.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quizyfy.quizapp.Entities.Question;
+import com.quizyfy.quizapp.Entities.Quiz;
 import com.quizyfy.quizapp.services.QuizService;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
@@ -18,13 +24,18 @@ public class QuizController {
     QuizService quizService;
 
     // http://localhost:8080/quiz/create?category=java&noOfQuestion=5&title=jQuiz
-    @GetMapping("create")
+    @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int noOfQuestion,
             @RequestParam String title) {
 
         quizService.createQuiz(category, noOfQuestion, title);
 
         return new ResponseEntity<>("Success ", HttpStatus.CREATED);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<Question>> getQuizQuestions(@PathVariable Integer id) {
+
     }
 
 }
